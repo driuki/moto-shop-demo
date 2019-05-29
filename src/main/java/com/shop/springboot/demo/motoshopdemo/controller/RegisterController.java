a83e0345bb275c32c19bf57f5101fdf6fccb1635
unknown <dargav999@gmail.com>
2019-05-29 14:54:09 +0300

    Change

--- src/main/java/com/shop/springboot/demo/motoshopdemo/controller/RegisterController.java
+++ src/main/java/com/shop/springboot/demo/motoshopdemo/controller/RegisterController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.shop.springboot.demo.motoshopdemo.entity.Users;
-import com.shop.springboot.demo.motoshopdemo.service.UserRegisterService;
 import com.shop.springboot.demo.motoshopdemo.user.CrmUserRegister;
 
 @Controller
@@ -24,8 +23,8 @@ public class RegisterController {
 	
 //	ERROR
 //	Error creating bean with name 'registerController': Unsatisfied dependency expressed through field 'userRegisterService';
-	@Autowired
-	private UserRegisterService userRegisterService;
+//	@Autowired
+//	private UserRegisterService userRegisterService;
 	
 	private Logger logger = Logger.getLogger(getClass().getName());
 	
@@ -47,33 +46,33 @@ public class RegisterController {
 		
 	}
 	
-	@PostMapping("/processRegister")
-	public String registerPost(@Valid @ModelAttribute("crmUser") CrmUserRegister theCrmUserRegister,
-									BindingResult theBindingResult, Model theModel) {
-		
-		String user = theCrmUserRegister.getUsername();
-		logger.info("Registration of user: " + user);
-		
-		if (theBindingResult.hasErrors()) {
-			return "register";
-		}
-		
-		Users isExisting = userRegisterService.findByUserName(user);
-		
-		if (isExisting != null) {
-			theModel.addAttribute("crmUser", new CrmUserRegister());
-			theModel.addAttribute("registrationError", "User already exists");
-			
-			logger.warning("User already exists");
-			return "register";
-		}
-		
-		userRegisterService.save(theCrmUserRegister);
-		
-		logger.info("Successfully created user" + user);
-		
-		return "/";
-		
-	}
+//	@PostMapping("/processRegister")
+//	public String registerPost(@Valid @ModelAttribute("crmUser") CrmUserRegister theCrmUserRegister,
+//									BindingResult theBindingResult, Model theModel) {
+//		
+//		String user = theCrmUserRegister.getUsername();
+//		logger.info("Registration of user: " + user);
+//		
+//		if (theBindingResult.hasErrors()) {
+//			return "register";
+//		}
+//		
+//		Users isExisting = userRegisterService.findByUserName(user);
+//		
+//		if (isExisting != null) {
+//			theModel.addAttribute("crmUser", new CrmUserRegister());
+//			theModel.addAttribute("registrationError", "User already exists");
+//			
+//			logger.warning("User already exists");
+//			return "register";
+//		}
+//		
+//		userRegisterService.save(theCrmUserRegister);
+//		
+//		logger.info("Successfully created user" + user);
+//		
+//		return "/";
+//		
+//	}
 	
 }
